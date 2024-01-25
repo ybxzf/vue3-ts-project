6c9f60d0f9938d1224e9da74ee59d3475cef7b75
ybxzf <42826051+ybxzf@users.noreply.github.com>
2024-01-25 20:43:42 +0800

    Create index.vue

---
+++ src/views/piniaTest/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="page">
+    <value-change></value-change>
+  </div>
+</template>
+<script setup lang="ts">
+import valueChange from "./components/valueChange.vue";
+</script>
+<style lang="less" scoped>
+.page {
+  height: 100%;
+  width: 100%;
+}
+</style>
