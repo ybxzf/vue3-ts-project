ca240c21e42d56a31710f9a6de58a0e35b1dab7c
ybxzf <42826051+ybxzf@users.noreply.github.com>
2024-01-25 20:37:53 +0800

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,31 @@
-import './assets/main.css'
+import './assets/base.less';
 
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
+import { createApp } from 'vue';
+
+//安装pinia
+import { createPinia } from 'pinia';
 import piniaPluginPersistedstate from "pinia-plugin-persistedstate";
-
-import App from './App.vue'
-import router from './router'
-
-const app = createApp(App)
-
 const pinia = createPinia();
 pinia.use(piniaPluginPersistedstate);
 
-app.use(router)
-app.use(pinia)
+import App from './App.vue';
 
-app.mount('#app')
+//安装element-plus
+import ElementPlus from 'element-plus';
+import 'element-plus/dist/index.css';
+import * as ElementPlusIconsVue from '@element-plus/icons-vue';
+
+//引入router
+import router from './router';
+
+const app = createApp(App);
+
+app.use(router);
+app.use(pinia);
+app.use(ElementPlus);
+
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+    app.component(key, component)
+};
+
+app.mount('#app');
