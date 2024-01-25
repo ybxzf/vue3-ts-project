1c25107150c0b68ecd259d441c891a3b9a80ec54
ybxzf <42826051+ybxzf@users.noreply.github.com>
2024-01-25 20:41:56 +0800

    Update routes.ts

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -1,21 +1,50 @@
 // import { RouteRecordRaw } from "vue-router";
-import HomeView from '@/views/HomeView.vue'
+import Home from "@/views/Home.vue";
 
 export const routes: any = [
   {
-    path: '/home',
-    name: 'home',
-    component: HomeView
+    path: '/',
+    redirect: '/home',
   },
   {
-    path: '/about',
-    name: 'about',
-    component: () => import('@/views/AboutView.vue')
-  }
-  ,
-  {
-    path: '/piniaTest',
-    name: 'piniaTest',
-    component: () => import('@/views/PiniaTestView.vue')
+    path: '/home',
+    name: 'home',
+    redirect: "/piniaTest",
+    component: Home,
+    children: [
+      {
+        path: '/piniaTest',
+        name: 'piniaTest',
+        cName: 'pinia测试',
+        component: () => import('@/views/piniaTest/index.vue')
+      },
+      {
+        path: '/threeJs',
+        name: 'threeJs',
+        cName: 'three.js案例',
+        redirect: "/threeJs/three01",
+        // component: () => import('@/views/threeJs/index.vue'),
+        children: [
+          {
+            path: '/threeJs/three01',
+            name: 'threeJs/three01',
+            cName: '案例1',
+            component: () => import('@/views/threeJs/components/three01.vue')
+          },
+          {
+            path: '/threeJs/three02',
+            name: 'threeJs/three02',
+            cName: '案例2',
+            component: () => import('@/views/threeJs/components/three02.vue')
+          },
+        ]
+      },
+      {
+        path: '/about',
+        name: 'about',
+        cName: '关于',
+        component: () => import('@/views/AboutView.vue')
+      },
+    ]
   }
 ];
