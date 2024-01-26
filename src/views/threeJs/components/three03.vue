f8963d32e3742bbffc0676ea3baf7da78af59d31
ybxzf <42826051+ybxzf@users.noreply.github.com>
2024-01-26 17:34:23 +0800

    Create three03.vue

---
+++ src/views/threeJs/components/three03.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="page">
+    <div class="three-ctn" ref="myThree"></div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { ref, onMounted } from "vue";
+import { render } from "@/cases/three/threeCase03";
+
+const myThree = ref<any>();
+
+onMounted(() => {
+  //设置渲染器大小
+  render.setSize(1000, 800);
+  myThree.value?.appendChild(render.domElement);
+});
+</script>
+
+<style lang="less" scoped></style>
