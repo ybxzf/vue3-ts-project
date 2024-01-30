a1e525e1edfab5b15933095c300ea86f907027f6
ybxzf <42826051+ybxzf@users.noreply.github.com>
2024-01-30 17:17:23 +0800

    Update cubeMesh.ts

--- src/cases/three/threeModules/case05/components/cubeMesh.ts
+++ src/cases/three/threeModules/case05/components/cubeMesh.ts
@@ -1,16 +1,21 @@
-import { BoxGeometry, Mesh, MeshStandardMaterial } from 'three';
+import { BoxGeometry, Mesh, MeshStandardMaterial, MeshBasicMaterial, TextureLoader } from 'three';
 
 /**
  * 封装网格模型的方法，包括创建形状和材质，并对网格模型及其设置
  * @returns 一个网格模型对象
  */
+const textureUrl = new URL(`@/assets/logo.svg`, import.meta.url).href
 function createCubeMash(x: number = 0, y: number = 0, z: number = 0) {
+  //引入纹理
+  // const texture: any = new TextureLoader().load('https://mujinzjh.github.io/img/404.jpg');
+  const texture: any = new TextureLoader().load(textureUrl);
   //创建几何形状
   const geometry: any = new BoxGeometry(2, 2, 2);
   //创建材质，不使用MeshBasicMaterial基础材质了，这个材质需要光源要求
   const material: any = new MeshStandardMaterial({
-    color: 'purple', //(紫色)
-    transparent: true, //开启透明度
+    map: texture,
+    // color: 'purple', //(紫色)
+    // transparent: true, //开启透明度
     opacity: 0.9, //设置透明度 
   });
 
